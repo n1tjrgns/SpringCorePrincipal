56dcfa9eeddbe6628a70ab0bb0104a6968dd8f93
seokhun <n2tjrgns@gmail.com>
2020-10-02 16:27:53 +0900

    AppConfig 리팩토링

--- src/main/java/com/spring/principal/AppConfig.java
+++ src/main/java/com/spring/principal/AppConfig.java
@@ -1,6 +1,8 @@
 package com.spring.principal;
 
+import com.spring.principal.discount.DiscountPolicy;
 import com.spring.principal.discount.FixDiscountPolicy;
+import com.spring.principal.member.MemberRepository;
 import com.spring.principal.member.MemberService;
 import com.spring.principal.member.MemberServiceImpl;
 import com.spring.principal.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.spring.principal.order.OrderServiceImpl;
 public class AppConfig {
 
     //생성자 주입
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
