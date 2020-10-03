20f4a8decb6f226aae1c288f8817de476c77cde0
seokhun <n2tjrgns@gmail.com>
2020-10-03 14:49:32 +0900

    스프링 컨테이너에 등록한 bean 확인하는 방법

---
+++ src/test/java/com/spring/principal/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,38 @@
+package com.spring.principal.beanfind;
+
+import com.spring.principal.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("컨테이너에 등록된 스프링 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("내가 등록한 어플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //ROLE_APPLICATION : 내가 어플리케이션에 추가적으로 등록한 빈
+            //ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
