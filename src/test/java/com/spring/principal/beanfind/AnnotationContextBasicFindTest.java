c93e4c63b0ff0c9a3e60f659fcc8e47cd9c07372
seokhun <n2tjrgns@gmail.com>
2020-10-03 15:19:29 +0900

    스프링 빈을 조회하는 방법 - 기본

---
+++ src/test/java/com/spring/principal/beanfind/AnnotationContextBasicFindTest.java
@@ -0,0 +1,40 @@
+package com.spring.principal.beanfind;
+
+import com.spring.principal.AppConfig;
+import com.spring.principal.member.MemberService;
+import com.spring.principal.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.boot.convert.DurationFormat;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnnotationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("해당 빈이 없는경우 예외 발생")
+    void findBeanByNameX(){
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
